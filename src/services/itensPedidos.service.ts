c03553232b0c92274140831ef8a239bc4b7ad4e3
MANOELA LEITE DE CAMPOS <manoela.campos2@senaisp.edu.br>
2026-06-11 08:15:25 -0300

    feat: adiciona service de devolução de item

--- src/services/itensPedidos.service.ts
+++ src/services/itensPedidos.service.ts
@@ -5,17 +5,21 @@ import { IItensPedido } from "../models/itensPedido.model";
 export class ItensPedidosService {
     constructor(private _repository = new ItensPedidoRepository()) {}
 
+  async devolverItem(id_itens_pedido: number) {
+    return await this._repository.devolverItem(id_itens_pedido);
+  }
 
-  // async editarItensPedido(quantidade:number, status:string, id_pedido: number, id_produto:number, id_itens_pedido: number) {
-  //   const itenspedidos =ItensPedido.editarItensPedidos(
-  //     quantidade,
-  //     id_produto,
-  //     status,
-  //     id_pedido,
-  //   );
-  //   return await this._repository.update(id_itens_pedido, itenspedidos);
+  async editarItensPedido(quantidade:number, status:string, id_pedido: number, id_produto:number, id_itens_pedido: number) {
+   const itensPedido: IItensPedido = {
+    id_itens_pedido,
+    quantidade,
+    status,
+    id_pedido,
+    id_produto,
+  };
 
-  // }
+  return await this._repository.update(id_itens_pedido, itensPedido);
+}
   // insertMovimentações: async (pTelefone, pIdCliente) => {
   //       const connection = await pool.getConnection();
   //       try {
