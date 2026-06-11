0898fcc447bcb4e7e704fef2c070f10c8362bf50
MANOELA LEITE DE CAMPOS <manoela.campos2@senaisp.edu.br>
2026-06-11 08:27:43 -0300

    feat: adiciona repository de movimentações de estoque

--- src/repository/movimentacao.repository.ts
+++ src/repository/movimentacao.repository.ts
@@ -1,5 +1,6 @@
 import { db } from "../database/connection.database";
 import { IMovimentacao } from "../models/movimentacao.model";
+import { ResultSetHeader } from "mysql2";
 
 export class MovimentacaoRepository {
     async findAll(): Promise<IMovimentacao[]> {
@@ -13,4 +14,21 @@ async findBystatus(status: string): Promise<IMovimentacao[]> {
     return rows;
 }
 
+async create(
+    dados: IMovimentacao,
+  ): Promise<ResultSetHeader> {
+    const sql = `
+      INSERT INTO movimentacoes (quantidade, status, id_itens_pedido)
+      VALUES (?,?,?);
+    `;
+
+    const values = [
+      dados.quantidade,
+      dados.status,
+      dados.id_itens_pedido,
+    ];
+
+    const [rows] = await db.execute<ResultSetHeader>(sql, values);
+    return rows;
+  }
 };
